5c4282774786bdecc60d1c6087365258d581c973
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2020-10-12 18:08:33 +0530

    added empty folders for new folder structure
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/core/Button/ButtonOutlined/ButtonOutlined.stories.tsx
+++ src/core/Button/ButtonOutlined/ButtonOutlined.stories.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { KuberaThemeProvider } from '../../../../theme';
+import { KuberaThemeProvider } from '../../../theme';
 import {
   ButtonOutlinedDefault,
   ButtonOutlinedHighlight,
--- src/core/Button/ButtonOutlined/__tests__/ButtonOutlined.test.tsx
+++ src/core/Button/ButtonOutlined/__tests__/ButtonOutlined.test.tsx
@@ -1,11 +1,14 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { KuberaThemeProvider } from '../../../../theme';
 import { ButtonOutlinedDefault } from '../../ButtonOutlined';
 
 describe('Button Outlined Component', () => {
   it('Renders', () => {
     const { container } = render(
-      <ButtonOutlinedDefault handleClick={() => {}} />
+      <KuberaThemeProvider platform="kubera-chaos">
+        <ButtonOutlinedDefault handleClick={() => {}} />
+      </KuberaThemeProvider>
     );
 
     const node = container.querySelector('global.buttonOutlined');
