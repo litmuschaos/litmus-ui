6c95300d7f9f9cc643e9f0a4c9ad3246ebe08011
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2020-10-12 10:17:32 +0530

    re-organised code in theme folder
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/components/Example/styles.ts
+++ src/components/Example/styles.ts
@@ -3,6 +3,7 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) => ({
   button: {
     backgroundColor: theme.palette.highlight,
+    color: theme.palette.text.secondary,
   },
 }));
 
