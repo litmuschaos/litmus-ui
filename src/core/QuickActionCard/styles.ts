29af814a8ff1cf468e1542932fee4bbef32e9834
Ritik Srivastava <ritik.srivastava@mayadata.io>
2021-02-19 17:19:54 +0530

    image height width set
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

--- src/core/QuickActionCard/styles.ts
+++ src/core/QuickActionCard/styles.ts
@@ -13,16 +13,14 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 400,
   },
   listItems: {
+    height: "1.3rem",
     margin: 0,
     padding: 0,
-    marginTop: theme.spacing(1),
-    "& p": {
-      color: theme.palette.text.primary,
-      paddingLeft: theme.spacing(2),
-      textDecoration: "none",
-      margin: 0,
-    },
+    marginTop: theme.spacing(2),
     "& img": {
+      height: "100%",
+      width: "2rem",
+      margin: "auto",
       color: theme.palette.highlight,
     },
   },
