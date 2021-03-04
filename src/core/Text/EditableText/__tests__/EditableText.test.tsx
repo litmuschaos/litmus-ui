089e43ef98097d3aaa9166f07c03fe524f02f23b
Sayan Mondal <sayan.mondal@mayadata.io>
2021-03-04 13:27:56 +0530

    Removed Comments
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- src/core/Text/EditableText/__tests__/EditableText.test.tsx
+++ src/core/Text/EditableText/__tests__/EditableText.test.tsx
@@ -1,4 +1,3 @@
-// import { screen } from "@testing-library/react";
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { LitmusThemeProvider } from "../../../../theme";
