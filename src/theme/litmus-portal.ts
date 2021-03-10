5b846518a2a46bedb54fbfcb88704c3fd16465ef
Ritik Srivastava <ritik.srivastava@mayadata.io>
2021-03-10 18:51:53 +0530

    color updated as per new Litmus theme
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

--- src/theme/litmus-portal.ts
+++ src/theme/litmus-portal.ts
@@ -1,5 +1,4 @@
 import { createTheme } from "./base";
-
 const litmusPortalTheme = createTheme({
   palette: {
     primary: {
@@ -9,7 +8,7 @@ const litmusPortalTheme = createTheme({
     },
     secondary: {
       main: "#109B67",
-      light: "#858CDD",
+      light: "#109B6799",
       dark: "#128359",
     },
     graph: {
@@ -37,7 +36,7 @@ const litmusPortalTheme = createTheme({
         "#A0519573",
         "#A4508773",
         "#CC556A73",
-        "#FFA60073",
+        " #FFA60073",
         "#DFA73E73",
         "#F6793E73",
         "#6DA96673",
@@ -57,21 +56,21 @@ const litmusPortalTheme = createTheme({
     },
     warning: {
       light: "#F6B92B20",
-      main: "#F6B92B",
+      main: "#DBA017",
       dark: "#402C01",
     },
     background: {
-      default: "#FAFBFD",
+      default: "#F5F6F8",
       paper: "#FFFFFF",
     },
-    sidebarMenu: "#FAFBFD",
+    sidebarMenu: "#F5F6F8",
     loginBackground: "#FFFFFF",
-    disabledBackground: "#E6E6E6",
+    disabledBackground: "#D9D9D9",
     text: {
-      primary: "#101217",
+      primary: "#000000CC",
       secondary: "#FFFFFF",
-      disabled: "#777777",
-      hint: "#777777",
+      disabled: "#00000061",
+      hint: "#00000099",
     },
     highlight: "#5B44BA",
     horizontalStepper: {
@@ -80,7 +79,7 @@ const litmusPortalTheme = createTheme({
       pending: "#B9B9B9",
     },
     border: {
-      main: "#B9B9B9",
+      main: "#BBBBBB",
       success: "#109B67",
       error: "#CA2C2C",
     },
@@ -88,16 +87,16 @@ const litmusPortalTheme = createTheme({
       "linear-gradient(90.43deg, #5B44BA 0.35%, #858CDD 51.03%, #109B67 99.64%)",
     status: {
       running: {
-        text: "#F6FB92B",
-        background: "#F6FB92B20",
+        text: "#5252F6",
+        background: "#5252F620",
       },
       completed: {
         text: "#109B67",
         background: "#109B6720",
       },
       pending: {
-        text: "#B9B9B9",
-        background: "#B9B9B920",
+        text: "#BBBBBB",
+        background: "#BBBBBB20",
       },
       failed: {
         text: "#CA2C2C",
@@ -111,5 +110,4 @@ const litmusPortalTheme = createTheme({
     },
   },
 });
-
 export { litmusPortalTheme };
