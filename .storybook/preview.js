ded05be46941bef598bbb14c895f1048f740b3dd
Ritik Srivastava <ritik.srivastava@mayadata.io>
2021-03-13 03:19:38 +0530

    setting up buttonFilled mdx story
    Signed-off-by: Ritik Srivastava <ritik.srivastava@mayadata.io>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import React from "react";
+import { LitmusThemeProvider } from "../src/theme/ThemeProvider";
 
+export const decorators = [
+  (Story) => (
+    <LitmusThemeProvider>
+      <Story />
+    </LitmusThemeProvider>
+  ),
+];
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
