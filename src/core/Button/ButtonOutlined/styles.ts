733930e40f80bca6c29b2eb4c38587c5d6af4066
arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>
2021-04-01 18:34:47 +0530

    removed box-shadow from outline button according to design
    Signed-off-by: arkajyotiMukherjee <arkajyoti.mukherjee@mayadata.io>

--- src/core/Button/ButtonOutlined/styles.ts
+++ src/core/Button/ButtonOutlined/styles.ts
@@ -1,4 +1,4 @@
-import { fade, makeStyles, Theme } from "@material-ui/core";
+import { makeStyles, Theme } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
   // Global
@@ -9,17 +9,6 @@ const useStyles = makeStyles((theme: Theme) => ({
     background: "transparent",
     fontSize: "0.75rem",
     borderColor: theme.palette.highlight,
-    "&:hover": {
-      borderColor: theme.palette.highlight,
-      background: "transparent",
-      boxShadow: `0px 4px 5px -2px ${fade(
-        theme.palette.highlight,
-        0.2
-      )},0px 7px 10px 1px ${fade(
-        theme.palette.highlight,
-        0.14
-      )},0px 2px 16px 1px ${fade(theme.palette.highlight, 0.12)}`,
-    },
     "&:disabled": {
       borderColor: theme.palette.disabledBackground,
       color: theme.palette.text.disabled,
