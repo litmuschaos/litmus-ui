14eb2003132ae60b91625717f42ec4b99ec41ee8
Ritik Srivastava <ritik@chaosnative.com>
2021-05-24 18:05:22 +0530

    comments added for the base
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>

--- src/lab/Graphs/CalendarHeatmap/base.ts
+++ src/lab/Graphs/CalendarHeatmap/base.ts
@@ -1,7 +1,9 @@
 export interface GenericValue {
+  // Generic structure
   [index: string]: any;
 }
 export interface ToolTip<T> {
+  // Generic data for Tooltip
   data: T;
 }
 
@@ -9,9 +11,14 @@ export type ToolTipDateValue = ToolTip<GenericValue>;
 export type TooltipData = ToolTipDateValue;
 
 export interface DayData {
+  // Value is the variable used for the internal calculation
+  // of the colors
   value: number;
+
+  // Other data that belongs to the structure
   [index: string]: any;
 }
 export interface WeekData {
+  // Bins corresonds to the bin of 7 days combined
   bins: DayData[];
 }
