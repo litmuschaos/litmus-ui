10133a388dbec6029a8ee019896ab4a7905ce755
Vansh Bhatia <vansh@chaosnative.com>
2021-07-06 17:20:00 +0530

    Snackbar (#97)
    * Snackbar added close button
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>
    
    * Snackbar tests added
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>
    
    * Snackbar default state added
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>
    
    * Snackbar handleClose issue fixed
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>
    
    * Snackbar state management issue fixed
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>
    
    * Review addressed
    
    Signed-off-by: Vansh Bhatia <vansh@chaosnative.com>

---
+++ src/core/Snackbar/Snackbar.tsx
@@ -0,0 +1,75 @@
+import { Snackbar as MuiSnackbar } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import { SnackbarProps as SnackbarBaseProps } from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import React from "react";
+import { useStyles } from "./styles";
+
+type Variant = "success" | "warning" | "error" | undefined;
+
+export interface SnackbarProps extends SnackbarBaseProps {
+  variant?: Variant;
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Snackbar: React.FC<SnackbarProps> = ({
+  variant,
+  children,
+  className,
+  setOpen,
+  open,
+  message,
+  ...rest
+}) => {
+  // Styles
+  const classes = useStyles();
+
+  const handleClose = (
+    event: React.SyntheticEvent | MouseEvent,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen?.(false);
+  };
+
+  function getVariant(variant: Variant): string {
+    switch (variant) {
+      case "success":
+        return classes.success;
+      case "warning":
+        return classes.warning;
+      case "error":
+        return classes.error;
+      default:
+        return classes.default;
+    }
+  }
+
+  return (
+    <MuiSnackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      message={message}
+      open={open}
+      onClose={handleClose}
+      autoHideDuration={2000}
+      action={
+        <>
+          <IconButton className={classes.IconButton} onClick={handleClose}>
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        </>
+      }
+      className={`${className} ${getVariant(variant)}`}
+      {...rest}
+    >
+      {children}
+    </MuiSnackbar>
+  );
+};
+
+export { Snackbar };
