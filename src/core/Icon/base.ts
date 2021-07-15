6a11ba3cacc9104f851dda58e4cc2486818ebf0b
Ritik Srivastava <ritik@chaosnative.com>
2021-07-15 16:39:37 +0530

    Icon export fix (#107)
    * assets folder moved to src, iconBundle added
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * Interface name change
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * jest config update
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * jest config update-2
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * package version update to 1-4-2
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * raw-loader removed
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>
    
    * getIcon function added
    
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>

--- src/core/Icon/base.ts
+++ src/core/Icon/base.ts
@@ -1,4 +1,13 @@
-export type IconSize = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
+export type IconSize =
+  | "xs"
+  | "sm"
+  | "md"
+  | "lg"
+  | "xl"
+  | "2xl"
+  | "3xl"
+  | "4xl"
+  | "5xl";
 
 export type IconName =
   | "agents"
