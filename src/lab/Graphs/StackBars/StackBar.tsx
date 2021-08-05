ef3fe9d11fc80add7cc4bfe29b2cf87eb5f237c4
Ritik Srivastava <ritik@chaosnative.com>
2021-08-05 11:35:42 +0530

    stack bar error fix
    Signed-off-by: Ritik Srivastava <ritik@chaosnative.com>

--- src/lab/Graphs/StackBars/StackBar.tsx
+++ src/lab/Graphs/StackBars/StackBar.tsx
@@ -4,10 +4,10 @@ import { BarStackProps } from "./base";
 import { FilteredStackBar } from "./FilteredStackBar";
 
 const StackBar: React.FC<BarStackProps> = ({ ...rest }) => {
+  //  ParentSize calculates the (width,height) of the parent and passes
+  //  it to the FilteredStackBar along with other props
   return (
     <ParentSize>
-      {/* ParentSize calculates the (width,height) of the parent and passes  */}
-      {/* it to the FilteredStackBar along with other props */}
       {({ width, height }) =>
         width > 0 &&
         height > 0 && (
