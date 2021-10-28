be53c99e0a3c7ff2e59c81c9747d73e279462273
Sayan Mondal <sayan@chaosnative.com>
2021-10-28 16:33:27 +0530

    Adding UTs for Button Filled
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- src/core/Button/ButtonFilled/__tests__/ButtonFilled.test.tsx
+++ src/core/Button/ButtonFilled/__tests__/ButtonFilled.test.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
 import React from "react";
 import { LitmusThemeProvider } from "../../../../theme";
 import { ButtonFilled } from "../ButtonFilled";
@@ -12,5 +13,25 @@ describe("Button Filled Component", () => {
     );
 
     expect(getByText("Button Filled")).toBeTruthy();
+  });
+
+  it("should be clickable", () => {
+    const mockCallBack = jest.fn();
+    const { getByText } = render(
+      <LitmusThemeProvider>
+        <ButtonFilled onClick={mockCallBack}>Button Filled</ButtonFilled>
+      </LitmusThemeProvider>
+    );
+
+    const Button = getByText("Button Filled");
+
+    // Clicking once
+    fireEvent.click(Button);
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+
+    // Clicking twice
+    fireEvent.click(Button);
+    fireEvent.click(Button);
+    expect(mockCallBack.mock.calls.length).toEqual(3); // Total three clicks registered on DOM
   });
 });
