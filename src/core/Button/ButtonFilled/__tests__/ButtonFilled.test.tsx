36f95e40e2bb79e6ae13f3367fba040b8e1fcf9c
Sayan Mondal <sayan@chaosnative.com>
2021-11-03 14:46:02 +0530

    Adding UTs for Button Filled (#124)
    * Adding UTs for Button Filled
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Updated Unit Test Approach
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>
    
    * Removing redundant initialization
    
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- src/core/Button/ButtonFilled/__tests__/ButtonFilled.test.tsx
+++ src/core/Button/ButtonFilled/__tests__/ButtonFilled.test.tsx
@@ -1,16 +1,36 @@
-import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { LitmusThemeProvider } from "../../../../theme";
 import { ButtonFilled } from "../ButtonFilled";
 
-describe("Button Filled Component", () => {
-  it("Renders", () => {
-    const { getByText } = render(
-      <LitmusThemeProvider>
-        <ButtonFilled onClick={() => {}}>Button Filled</ButtonFilled>
-      </LitmusThemeProvider>
-    );
+let component: HTMLElement;
+let mockCallBack = jest.fn();
 
-    expect(getByText("Button Filled")).toBeTruthy();
-  });
+beforeEach(() => {
+  render(
+    <LitmusThemeProvider>
+      <LitmusThemeProvider>
+        <ButtonFilled onClick={mockCallBack}>Button Filled</ButtonFilled>
+      </LitmusThemeProvider>
+    </LitmusThemeProvider>
+  );
+
+  // Get Button Filled component
+  component = screen.getByText("Button Filled");
+});
+
+test("Renders", () => {
+  expect(component).toBeTruthy();
+});
+
+test("should be clickable", () => {
+  // Clicking once
+  fireEvent.click(component);
+  expect(mockCallBack.mock.calls.length).toEqual(1);
+
+  // Clicking twice
+  fireEvent.click(component);
+  fireEvent.click(component);
+  expect(mockCallBack.mock.calls.length).toEqual(3); // Total three clicks registered on DOM
 });
