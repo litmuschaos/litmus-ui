5b13e19f1471063dcd226ba6927c28c34eaa8c20
Sayan Mondal <sayan@chaosnative.com>
2021-11-12 14:26:45 +0530

    Adding new icons and updating Modal component
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- src/core/Icon/iconBundle.ts
+++ src/core/Icon/iconBundle.ts
@@ -1,4 +1,5 @@
 import agents from "../../assets/icons/agents.svg";
+import alert from "../../assets/icons/alert.svg";
 import analytics from "../../assets/icons/analytics.svg";
 import applicationDashboard from "../../assets/icons/applicationDashboard.svg";
 import arrowDown from "../../assets/icons/arrowDown.svg";
@@ -7,21 +8,25 @@ import arrowRight from "../../assets/icons/arrowRight.svg";
 import arrowUp from "../../assets/icons/arrowUp.svg";
 import backward from "../../assets/icons/backward.svg";
 import calendar from "../../assets/icons/calendar.svg";
+import chaoshub from "../../assets/icons/chaoshub.svg";
 import check from "../../assets/icons/check.svg";
 import chevronDown from "../../assets/icons/chevronDown.svg";
 import chevronLeft from "../../assets/icons/chevronLeft.svg";
 import chevronRight from "../../assets/icons/chevronRight.svg";
 import chevronUp from "../../assets/icons/chevronUp.svg";
+import chooseAgent from "../../assets/icons/chooseAgent.svg";
 import clock from "../../assets/icons/clock.svg";
 import close from "../../assets/icons/close.svg";
 import closeWrapped from "../../assets/icons/closeWrapped.svg";
 import clusters from "../../assets/icons/clusters.svg";
 import code from "../../assets/icons/code.svg";
+import codeFilled from "../../assets/icons/codeFilled.svg";
 import collapseDown from "../../assets/icons/collapseDown.svg";
 import collapseLeft from "../../assets/icons/collapseLeft.svg";
 import collapseRight from "../../assets/icons/collapseRight.svg";
 import collapseUp from "../../assets/icons/collapseUp.svg";
 import community from "../../assets/icons/community.svg";
+import computer from "../../assets/icons/computer.svg";
 import configure from "../../assets/icons/configure.svg";
 import copy from "../../assets/icons/copy.svg";
 import dataSource from "../../assets/icons/dataSource.svg";
@@ -29,6 +34,9 @@ import disableSchedule from "../../assets/icons/disableSchedule.svg";
 import disconnectTarget from "../../assets/icons/disconnectTarget.svg";
 import document from "../../assets/icons/document.svg";
 import download from "../../assets/icons/download.svg";
+import drive from "../../assets/icons/drive.svg";
+import dropbox from "../../assets/icons/dropbox.svg";
+import editSequence from "../../assets/icons/editSequence.svg";
 import enableSchedule from "../../assets/icons/enableSchedule.svg";
 import expand from "../../assets/icons/expand.svg";
 import expandDown from "../../assets/icons/expandDown.svg";
@@ -38,6 +46,7 @@ import expandUp from "../../assets/icons/expandUp.svg";
 import experiment from "../../assets/icons/experiment.svg";
 import experimentError from "../../assets/icons/experimentError.svg";
 import experimentFailed from "../../assets/icons/experimentFailed.svg";
+import experimentFilled from "../../assets/icons/experimentFilled.svg";
 import experimentNotAvailable from "../../assets/icons/experimentNotAvailable.svg";
 import experimentOmitted from "../../assets/icons/experimentOmitted.svg";
 import experimentPassed from "../../assets/icons/experimentPassed.svg";
@@ -63,6 +72,7 @@ import project from "../../assets/icons/project.svg";
 import recurringSchedule from "../../assets/icons/recurringSchedule.svg";
 import refresh from "../../assets/icons/refresh.svg";
 import reload from "../../assets/icons/reload.svg";
+import rocket from "../../assets/icons/rocket.svg";
 import schedule from "../../assets/icons/schedule.svg";
 import scheduleWorkflow from "../../assets/icons/scheduleWorkflow.svg";
 import search from "../../assets/icons/search.svg";
@@ -93,6 +103,16 @@ interface AllIconsProps {
 // all the icons is constructed
 const allIcons: AllIconsProps = {
   agents,
+  alert,
+  rocket,
+  editSequence,
+  computer,
+  experimentFilled,
+  drive,
+  dropbox,
+  codeFilled,
+  chooseAgent,
+  chaoshub,
   analytics,
   applicationDashboard,
   arrowDown,
